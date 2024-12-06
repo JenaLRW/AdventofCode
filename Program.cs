214a2b07602bf86bbb81f3cb15c83f3949bccf53
Jena White <jenale@gmail.com>
2024-12-05 23:32:15 -0500

    first program update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] lines = File.ReadAllLines("day1inputdata.txt");
+
+List<int> leftValues = new List<int>();
+List<int> rightValues = new List<int>();
+
+
+foreach (string line in lines)
+{
+    string[] parts = line.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+    if (parts.Length >= 2 && !string.IsNullOrEmpty(parts[1]))
+    {
+        int left = int.Parse(parts[0]);
+        leftValues.Add(left);
+
+        int right = int.Parse(parts[1]);
+        rightValues.Add(right);
+
+    }
+      
+}
+
+leftValues.Sort();
+rightValues.Sort();
+
+for (int i = 0; i < leftValues.Count; i++)
+{
+Console.WriteLine($"{leftValues[i]}  {rightValues[i]}");
+}
+
+
